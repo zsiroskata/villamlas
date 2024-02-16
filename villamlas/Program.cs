06dd8ded188ba5d1ce931d1a35b77ab4d37924a9
zsiroskata <zsiros.kata02@gmail.com>
2024-02-16 10:41:08 +0100

    s

--- villamlas/Program.cs
+++ villamlas/Program.cs
@@ -4,7 +4,77 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			List<Villamlas> villamok = new();
+			using StreamReader sr = new StreamReader(
+				path: @"..\..\..\src\villam.txt",
+				encoding: System.Text.Encoding.UTF8
+				);
+			while ( !sr.EndOfStream )
+			{
+				villamok.Add(new Villamlas(sr.ReadLine()));
+			}
+
+            foreach (var item in villamok)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("3.feladat");
+            Console.WriteLine(legtobb(villamok));
+
+            Console.WriteLine("4.feladat");
+            negyes(villamok);
+
+            Console.WriteLine("5. feladat");
+        }
+
+		static int legtobb(List<Villamlas> villamok)
+		{
+			List<int> lista = new List<int>();
+			int max = 0;
+
+            foreach (var item in villamok)
+            {
+                for (int i = 0; i < item.Ora.Count; i++)
+                {
+					lista.Add(item.Ora[i]);
+                }
+            }
+
+			max = lista.Max() ;
+
+            return max;
 		}
+
+		static void negyes (List<Villamlas> villamok) 
+		{
+            for (int i = 0; i < villamok.Count; i++)
+            {
+                int sum = villamok[i].Ora.Sum();
+                for (int j = 0; j < villamok[i].Ora.Count; j++)
+                {
+                    if (sum > 0)
+                    {
+                        Console.WriteLine($"{i+1} nap,  óra");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{i+1} nap null");
+                        
+                    }
+                    break;
+                }
+            }
+        }
+
+        static int otos(List<Villamlas> villamok)
+        {
+            int szam = 0;
+            for (int i = 0; i < villamok.Count; i++)
+            {
+                int sum = villamok[i].Ora.Sum();
+            }
+
+		    return szam;
+        }
 	}
 }
---
+++ villamlas/Villamlas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace villamlas
+{
+	internal class Villamlas
+	{
+        public List<int> Ora { get; set; }
+
+        public Villamlas(string sorok) 
+        {
+            var sor = sorok.Split(';');
+            Ora = new List<int>();
+            foreach (var item in sor)
+            {
+                Ora.Add(int.Parse(item));
+            }
+        }
+        public override string ToString()
+        {
+            return $"{string.Join(" ", Ora)}";
+        }
+    }
+}
